cb1241b599ed85d2f2696e282fbce132fc7d2ab5
Adxell <adxell02@gmail.com>
2021-11-27 13:21:53 -0500

    update App

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,7 +1,14 @@
 import Navbar from './components/Nav'
+import SearchBar from './components/SearchBar';
 function App() {
   return (
+    <div>
+
     <Navbar/>
+    <div className="app">
+        <SearchBar/>
+    </div>
+    </div>
   )
 }
 
