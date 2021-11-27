a315b28e402ddc33417ec76586e87a3e931aad8e
Adxell <adxell02@gmail.com>
2021-11-27 13:19:39 -0500

    update SearchBar

--- pokemon/src/components/SearchBar.jsx
+++ pokemon/src/components/SearchBar.jsx
@@ -1,8 +1,28 @@
 import React from "react";
-
+import { useState } from "react";
+import  {searchPokemon}  from "../api";
 const SearchBar=()=>{
+    const [search, setSearch]=useState('');
+    const [pokemon, setPokemon]=useState();
+    
+    const onChange  =(e)=>{
+        setSearch(e.target.value)
+    }
+    const onClick = async()=>{
+        const data= await searchPokemon(search)
+        setPokemon(data)
+    }
     return (
-        <div>SearchBar</div>
+        <div className="search-bar-container">
+            <div className="serachbar">
+                <input type="text" 
+                onChange={onChange}
+                />
+            </div>
+            <div>
+                <button className="serachbar-btn" onClick={onClick}>Buscar</button>
+            </div>
+        </div>
     )
 }
 
