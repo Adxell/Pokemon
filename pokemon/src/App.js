7369b102d6aa0235484ec011c4e8ea52ec2d2656
Adxell <adxell02@gmail.com>
2021-11-29 19:29:15 -0500

    update App.js

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,12 +1,37 @@
 import Navbar from './components/Nav'
 import SearchBar from './components/SearchBar';
+import Result from './components/Results';
+import { useState, useEffect } from 'react';
+import { getPokemonData, getPokemos } from './api';
+
 function App() {
+  const [pokemon, setPokemon]=useState([])
+  //console.log(pokemon)
+  const fechPokemon= async ()=>{
+      try{
+        const data = await getPokemos();
+        //console.log(data)
+        const promises = data.results.map(async (poke) => {
+           return await getPokemonData(poke.url);
+        });
+        //console.log(promises)
+        const results= await Promise.all(promises);
+        setPokemon(results);
+        //console.log(results)
+       
+      }catch(err){
+        console.log(err);
+      }
+  }
+  useEffect(() => {
+    fechPokemon()
+  },[])
   return (
     <div>
-
     <Navbar/>
     <div className="app">
         <SearchBar/>
+        <Result pokemos={pokemon}/>
     </div>
     </div>
   )
