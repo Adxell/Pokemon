0d11f92828630fbe21184a9ffb1505322d3752d6
Adxell <adxell02@gmail.com>
2021-11-29 19:21:57 -0500

    update api

--- pokemon/src/api.js
+++ pokemon/src/api.js
@@ -8,3 +8,25 @@ export const searchPokemon = async (pokemon)=>{
         console.log(err)
     }
 }
+
+export const getPokemos= async (limit=10, offset=0)=>{
+      try {
+        let url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        return data;
+      } catch (err) {
+        console.log(err);
+      }
+}
+
+export const getPokemonData=async (url)=>{
+    try{
+        const res = await fetch(url);
+        const data= await res.json();
+        //console.log(data)
+        return data;
+    }catch(err){
+        console.log(err)
+    }
+}
